2af3fa44be96ae6d6ae4e1871b2bbf954f4e226f
JamDon2 <hello@jamdon2.dev>
2022-10-10 19:30:29 +0200

    Balance command

--- src/commands.ts
+++ src/commands.ts
@@ -1,15 +1,17 @@
 import { CommandInteraction, SlashCommandBuilder } from "discord.js";
 import { Collection } from "mongodb";
+import balance from "./commands/balance.js";
 import login from "./commands/login.js";
+import HordeDocument from "./types/document.js";
 
 export const commands: Omit<
     SlashCommandBuilder,
     "addSubcommandGroup" | "addSubcommand"
->[] = [login.command];
+>[] = [login.command, balance.command];
 
 export const commandHandlers: {
     [key: string]: (
         interaction: CommandInteraction,
-        collection: Collection
+        collection: Collection<HordeDocument>
     ) => Promise<void>;
-} = { login: login.handler };
+} = { login: login.handler, balance: balance.handler };
---
+++ src/commands/balance.ts
@@ -0,0 +1,52 @@
+import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { Collection } from "mongodb";
+
+import api from "../api/client.js";
+import HordeDocument from "../types/document.js";
+
+export default {
+    command: new SlashCommandBuilder()
+        .setName("balance")
+        .setDescription("Query your Kudos balance."),
+    async handler(
+        interaction: CommandInteraction,
+        collection: Collection<HordeDocument>
+    ) {
+        await interaction.deferReply({ ephemeral: true });
+
+        const user = await collection.findOne({ _id: interaction.user.id });
+
+        if (!user) {
+            await interaction.followUp(
+                "You are not logged in. Please use /login in the server."
+            );
+            return;
+        }
+
+        const userDetails = (await api
+            .get(`/users/${user.username.split("#")[1]}`)
+            .then((res) => res.data)
+            .catch((error) => {
+                if (error.response?.status === 404) {
+                    return { error: "username" };
+                }
+
+                return { error: "unknown" };
+            })) as { kudos: number } | { error: string };
+
+        if ("error" in userDetails) {
+            if (userDetails.error == "username") {
+                await interaction.followUp(
+                    "Invalid username. Try logging in again."
+                );
+            } else {
+                await interaction.followUp("An unknown error occurred.");
+            }
+            return;
+        }
+
+        await interaction.followUp(
+            `You have ${userDetails.kudos.toLocaleString("en-US")} Kudos.`
+        );
+    },
+};
