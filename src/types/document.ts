e2e0d9d6e1a7099ac5b124720937c165393005d8
JamDon2 <hello@jamdon2.dev>
2022-10-11 18:10:36 +0200

    Notification muting

--- src/types/document.ts
+++ src/types/document.ts
@@ -2,6 +2,10 @@ type HordeDocument = {
     _id: string;
     apiKey: string;
     username: string;
+    notifications?: {
+        send: number;
+        receive: number;
+    };
 };
 
 export default HordeDocument;
