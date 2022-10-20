7781ff9541ad447461a6ba79cf8c8f4e35172700
JamDon2 <hello@jamdon2.dev>
2022-10-20 13:54:14 +0200

    Fix redirect

--- src/index.ts
+++ src/index.ts
@@ -49,18 +49,18 @@ client.on("messageReactionAdd", async (reaction, user) => {
 
     user = user.partial ? await user.fetch() : user;
 
-    const recipientID = message.interaction
-        ? message.interaction.user.id
-        : message.author.id;
+    const author = message.interaction
+        ? message.interaction.user
+        : message.author;
 
-    if (recipientID === user.id) {
+    if (author.id === user.id) {
         await reaction.users.remove(user);
         return;
     }
 
     const sender = await User.findById(user.id);
 
-    const recipient = await User.findById(recipientID);
+    const recipient = await User.findById(author.id);
 
     if (!sender || !recipient) {
         if (!sender) {
@@ -77,12 +77,12 @@ client.on("messageReactionAdd", async (reaction, user) => {
         } else if (!recipient) {
             await new KudosEscrow({
                 from: user.id,
-                to: message.author.id,
+                to: author.id,
                 emoji: emojiIdentifier,
                 messageURL: message.url,
             }).save();
 
-            message.author
+            author
                 .createDM()
                 .then((dm) =>
                     dm.send(
@@ -100,7 +100,7 @@ client.on("messageReactionAdd", async (reaction, user) => {
                 .then((dm) =>
                     dm.send(
                         `<@${
-                            message.author.id
+                            author.id
                         }> is not logged in. If they log in within ${humanizeDuration(
                             config.escrowtime * 1000,
                             { largest: 2 }
@@ -135,7 +135,7 @@ client.on("messageReactionAdd", async (reaction, user) => {
         User,
         { id: user.id, apiKey: sender.apiKey, sendDM: sendEnabled },
         {
-            id: message.author.id,
+            id: author.id,
             username: recipient.username,
             sendDM: receiveEnabled,
         },
